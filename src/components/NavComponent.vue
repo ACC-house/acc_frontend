8f9ac7003818abe13f4f2a8ba1f5fe71335c7538
Angela-coello <angeladarko32@gmail.com>
2023-09-14 21:31:00 +0200

    next commit

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+
+</script>
+
+<template>
+
+  <div>
+   
+    <div>
+   
+    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent position-absolute w-100 top-0">
+      <div class="container">
+       
+        <a class="navbar-brand" href="#">
+          <img src="@/assets/logoacc.png" alt="Logo" width="125">
+        </a>
+
+        
+        <div class="ml-auto">
+          <button class="btn btn-outline-light mx-2 ">Contacto</button>
+        </div>
+      </div>
+    </nav>
+    </div>
+  </div>
+</template>
+
+
+
+
+
+<style scoped>
+
+
+body {
+  background-color: white;
+}
+
+
+.navbar {
+  z-index: 1; 
+}
+
+
+.custom-button {
+  border-color: gray; 
+}
+
+.container {
+  margin-top: 0;
+}
+
+</style>
+
+
+
+
+
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  
+
+   <div>
+    
+    <div class="d-flex justify-content-center align-items-center vh-100">
+      <img src="@/assets/photo.jpg" class="img-fluid" alt="Mi Foto">
+     
+    </div>
+    
+   
+  </div>
+  
+
+
 </template>
+
+<style scoped>
+
+  .titulo {
+  color: rgb(12, 105, 88); 
+  font-size: 30px; 
+}
+
+body {
+  background-color: white;
+  overflow-y: hidden;
+}
+
+.vh-100 {
+  min-height: 10vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
